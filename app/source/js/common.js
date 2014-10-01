22f13de39357d4b0feed03cf7e5f652ea2969f92
frontainer <frontainer@gmail.com>
2014-10-01 20:18:59 +0900

    Githubページ用に構成作成

--- app/source/js/common.js
+++
@@ -1,6 +0,0 @@
-require('jquery');
-require('angular');
-require('angular-route');
-require('angular-ui-ace');
-require('ngstorage');
-require('../lib/flexy-layout/flexy-layout.min');
--- app/source/js/main.js
+++
@@ -1,1 +0,0 @@
-require('./controllers/_RootCtrl');
